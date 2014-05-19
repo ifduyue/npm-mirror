7a72d245d5b640768b0d5846ca9da0cad3e40a90
Yue Du <ifduyue@gmail.com>
2014-05-19 16:03:23 +0800

    update DEFAULT_TO

--- lib/npm/mirror/config.rb
+++ lib/npm/mirror/config.rb
@@ -3,7 +3,7 @@ require 'tmpdir'
 module Npm
   module Mirror
     DEFAULT_FROM = 'http://registry.npmjs.org/'
-    DEFAULT_TO = Dir.mktmpdir 'npm-mirror-'
+    DEFAULT_TO = File.join(Dir.tmpdir, 'npm-mirror')
     DEFAULT_SERVER = 'http://localhost/'
   end
 end
